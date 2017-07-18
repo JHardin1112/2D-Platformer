141f63a58902a38f2084d9d6a337675ca9084f9d
Jeremy Hardin <jeremyhardin@gmail.com>
2017-07-18 17:14:13 -0400

    Moving platforms, rotating saws, player can stand on moving platforms, coins and coin pickups, particle effect when death occurs, spikes.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -73,4 +73,20 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "MovingPlatform")
+        {
+            transform.parent = other.transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "MovingPlatform")
+        {
+            transform.parent = null;
+        }
+    }
+
 }
